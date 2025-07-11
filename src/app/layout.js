fa2606d5cf13b9d82d9dc94c1dc6a97299ecc7cb
Raquel <silva.raquel2903@gmail.com>
2025-07-11 19:33:13 -0300

    adicionanado icone

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,11 +18,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 
         {children}
-        
+
       </body>
     </html>
   );
