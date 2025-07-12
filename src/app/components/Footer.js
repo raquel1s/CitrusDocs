add56e2784b18b10ec2d281cd520c0d0c94993aa
Julia Garcia <juliagabrieligarc@gmail.com>
2025-07-11 21:10:54 -0300

    Criando o footer

---
+++ src/app/components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer(){
+    return(
+        <footer className="flex flex-row justify-between bg-[var(--primary-color)] p-6 px-40 text-white font-bold text-center">
+            <p className="p-4">© 2025 CitrusDocs. Todos os direitos reservados.</p>
+            <div className=" g-2">
+                <p>30/05/2025 - 00/00/0000</p>
+                <p>Disciplina: Documentação de Sistemas</p>
+            </div>
+        </footer>
+    )
+}
