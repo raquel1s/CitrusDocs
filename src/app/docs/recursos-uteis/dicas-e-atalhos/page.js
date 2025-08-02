4e416320873c90e550d08386c8f03fb551a590b9
RAQUEL DA SILVA <raquel_silva10@estudante.sesisenai.org.br>
2025-08-01 21:13:59 -0300

    adicionando imagens

--- src/app/docs/recursos-uteis/dicas-e-atalhos/page.js
+++ src/app/docs/recursos-uteis/dicas-e-atalhos/page.js
@@ -1,4 +1,5 @@
 import DocLayout from "@/app/components/DocLayout";
+import Image from 'next/image';
 
 export default function DicasAtalhos(){
     return(
@@ -6,11 +7,39 @@ export default function DicasAtalhos(){
             <h1>Dicas e Atalhos</h1>
             <ul>
                 <li>Utilize Visualização após cada edição para checar o funcionamento;</li>
+                <Image
+                    className="pt-5 pb-10"
+                    src="/usar-visualizar.png"
+                    alt="Campo de seleção de curso"
+                    width={250}
+                    height={150}
+                />
                 <li>Faça backups regulares do formulário;</li>
+                <Image
+                    className="pt-5 pb-10"
+                    src="/fazer-backups.png"
+                    alt="Campo de seleção de curso"
+                    width={350}
+                    height={150}
+                />
                 <li>Evite edições simultâneas;</li>
                 <li>Duplique seções semelhantes para ganhar agilidade;</li>
+                <Image
+                    className="pt-5 pb-10"
+                    src="/duplicar-sessao.png"
+                    alt="Campo de seleção de curso"
+                    width={750}
+                    height={150}
+                />
                 <li>Registre grandes alterações em uma planilha de controle.</li>
                 <li>Padronize nomes: ex. "MI80" e não "mi80";</li>
+                <Image
+                    className="pt-5 pb-10"
+                    src="/padronizar-nomes.png"
+                    alt="Campo de seleção de curso"
+                    width={250}
+                    height={150}
+                />
             </ul>
         </DocLayout>
     )
--- src/app/docs/recursos-uteis/erros-comuns-e-solucoes/page.js
+++ src/app/docs/recursos-uteis/erros-comuns-e-solucoes/page.js
@@ -1,4 +1,5 @@
 import DocLayout from "@/app/components/DocLayout";
+import Image from 'next/image';
 
 export default function ErrosComuns(){
     return(
@@ -8,18 +9,46 @@ export default function ErrosComuns(){
             <h2>Formulário não redireciona</h2>
             <p><span>Causa comum: </span>Redirecionamento ausente/incorreto.</p>
             <p><span>Como Corrigir: </span>Ativar e revisar a opção “Ir para a seção com base na resposta”.</p>
+            <Image
+                className="pt-5 pb-10"
+                src="/form-nao-redireciona.png"
+                alt="Campo de seleção de curso"
+                width={550}
+                height={150}
+            />
 
             <h2>Representante não aparece</h2>
             <p><span>Causa comum: </span>Lista desatualizada</p>
             <p><span>Como corrigir: </span>Atualizar a pergunta múltipla escolha</p>
+            <Image
+                className="pt-5 pb-10"
+                src="/repre-nao-aparece.png"
+                alt="Campo de seleção de curso"
+                width={550}
+                height={150}
+            />
 
             <h2>Aluno não encontrado</h2>
             <p><span>Causa comum: </span>Campo “Alunos ausentes” desatualizado</p>
             <p><span>Como corrigir: </span>Inserir o nome na seção da turma</p>
+            <Image
+                className="pt-5 pb-10"
+                src="/aluno-nao-encontrado.png"
+                alt="Campo de seleção de curso"
+                width={550}
+                height={150}
+            />
 
             <h2>Professor ou UC ausente</h2>
             <p><span>Causa comum: </span>Grade incompleta</p>
             <p><span>Como corrigir: </span>Editar diretamente a grade e adicionar a UC/professor</p>
+            <Image
+                className="pt-5 pb-10"
+                src="/prof-nao-aparece.png"
+                alt="Campo de seleção de curso"
+                width={550}
+                height={150}
+            />
         </DocLayout>
     )
 }
